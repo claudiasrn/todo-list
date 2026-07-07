9c4b1fcee8dedac670f4b66d1bfe86bfe810d152
claudiasrn <perezserranoclaudia@gmail.com>
2026-07-08 01:18:47 +0200

    Write Task class

---
+++ src/models/Task.js
@@ -0,0 +1,15 @@
+export default class Task {
+  constructor(title, priority, description = "", dueDate = "", checklist = []) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.checklist = checklist;
+    this.completed = false;
+    this.id = this.id = crypto.randomUUID();
+  }
+
+  toggleComplete() {
+    this.completed = !this.completed;
+  }
+}
