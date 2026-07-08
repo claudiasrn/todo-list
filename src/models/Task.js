6c974b88570d8fd75c0984e60790b59be431d836
claudiasrn <perezserranoclaudia@gmail.com>
2026-07-08 02:09:18 +0200

    Add editTask, addChecklistItem and deleteChecklistItem methods

--- src/models/Task.js
+++ src/models/Task.js
@@ -12,4 +12,20 @@ export default class Task {
   toggleComplete() {
     this.completed = !this.completed;
   }
+
+  editTask(title, priority, description = "", dueDate = "", checklist = []) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.checklist = checklist;
+  }
+
+  addChecklistItem(name) {
+    this.checklist.push(new ListItem(name));
+  }
+
+  deleteChecklistItem(id) {
+     this.checklist = this.checklist.filter((i) => i.id !== id);
+  }
 }
