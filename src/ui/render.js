f612f89149c6e44132e678c796b0addc9a3a12f5
claudiasrn <perezserranoclaudia@gmail.com>
2026-07-09 22:29:52 +0200

    Persist app state to localStorage on every render

--- src/ui/render.js
+++ src/ui/render.js
@@ -2,12 +2,16 @@ import Application from "../state/Application.js";
 import { renderProjects } from "./renderProjects.js";
 import { renderEmptyState } from "./renderEmptyState.js";
 import { renderTasks } from "./renderTasks.js";
+import { saveToStorage } from "../state/storage.js";
 
 const app = document.querySelector("#app");
 
 export function render(application) {
     let activeProject;
     app.innerHTML = "";
+
+    saveToStorage("projects", application.projects);
+    saveToStorage("defaultProjectId", application.defaultProjectId)
     
     if (application.projects.length <= 0) {
         app.append( renderEmptyState(application) );
